f5f2608624f2e8c887fcb27be0ab805cacc0616f
Shafiq Shams <shafiqshamsessani@gmail.com>
2025-08-07 17:09:32 +0200

    feat: add todo mutation and handleAddTodo

--- src/DemoReactQuery.tsx
+++ src/DemoReactQuery.tsx
@@ -1,24 +1,45 @@
-import { useQuery } from "@tanstack/react-query";
-import { fetchTodos } from "./api/todos";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { addTodo, fetchTodos } from "./api/todos";
 import { TodoCard } from "./components/TodoCard";
+import { useState } from "react";
 
 export const DemoReactQuery = () => {
+  const [title, setTitle] = useState("");
   const { data: todos, isLoading } = useQuery({
     queryFn: () => fetchTodos(),
     queryKey: ["todos"],
   });
 
+  const { mutateAsync: addTodoMutation } = useMutation({
+    mutationFn: addTodo,
+  });
+
+  const handleAddTodo = async () => {
+    try {
+      await addTodoMutation({ title });
+      setTitle("");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  if (isLoading) return <h3>Loading...</h3>;
+
   return (
     <div className="container">
       <h2>DemoReactQuery</h2>
+      <input
+        type="text"
+        value={title}
+        placeholder="Write your todo"
+        onChange={(e) => setTitle(e.target.value)}
+      />
 
-      {isLoading ? (
-        <h3>Loading...</h3>
-      ) : (
-        todos?.map((todo) => {
-          return <TodoCard key={todo.id} todo={todo} />;
-        })
-      )}
+      <button onClick={handleAddTodo}>Add Todo</button>
+
+      {todos?.map((todo) => {
+        return <TodoCard key={todo.id} todo={todo} />;
+      })}
     </div>
   );
 };
