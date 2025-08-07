7eb890375c5360f837e9b035af7b70d5eb2f7932
Shafiq Shams <shafiqshamsessani@gmail.com>
2025-08-07 16:08:17 +0200

    feat(api): add mock functions to simulate actual fetching

---
+++ src/api/todos.ts
@@ -0,0 +1,31 @@
+/* Mock functions for fetching todos */
+
+import { todos } from "../mock_data/todos";
+import type { Todo } from "../types/Todo";
+
+export const fetchTodos = async (searchQuery: string = "") => {
+  //Simulate actual fetching from the API
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  const filteredTodos = todos.filter((todo) =>
+    todo.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  return [...filteredTodos];
+};
+
+export const addTodo = async (todo: Pick<Todo, "title">): Promise<Todo> => {
+  //Simulate actual fetching from the API
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  const newTodo: Todo = {
+    id: todos.length + 1,
+    title: todo.title,
+    completed: false,
+  };
+
+  // This is just to store in memory for the current execution
+  todos.push(newTodo);
+
+  return newTodo;
+};
