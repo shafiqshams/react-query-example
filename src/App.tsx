1328cbc82d45174de4d74c92a23b981a0c1cb023
Shafiq Shams <shafiqshamsessani@gmail.com>
2025-08-08 00:27:02 +0200

    feat: add toggle to show/hide component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import "./App.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { DemoReactQuery } from "./DemoReactQuery";
+import { useState } from "react";
 
 const queryClient = new QueryClient();
 
 function App() {
+  //Added for component mount & unmount
+  const [showDemo, setShowDemo] = useState(true);
   return (
     // To use this query client anywhere in the app through this context
     <QueryClientProvider client={queryClient}>
-      <DemoReactQuery />
+      <button onClick={() => setShowDemo(!showDemo)}>Toggle Demo</button>
+      {showDemo && <DemoReactQuery />}
     </QueryClientProvider>
   );
 }
