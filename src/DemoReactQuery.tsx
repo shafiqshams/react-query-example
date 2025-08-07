b11d7e23199bf13dc9f632df94a90273d016dc90
Shafiq Shams <shafiqshamsessani@gmail.com>
2025-08-07 16:33:05 +0200

    feat: add useQuery and data(todos) mapping

--- src/DemoReactQuery.tsx
+++ src/DemoReactQuery.tsx
@@ -1,3 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchTodos } from "./api/todos";
+import { TodoCard } from "./components/TodoCard";
+
 export const DemoReactQuery = () => {
-  return <div>DemoReactQuery</div>;
+  const { data: todos } = useQuery({
+    queryFn: () => fetchTodos(),
+    queryKey: ["todos"],
+  });
+
+  return (
+    <div className="container">
+      <h2>DemoReactQuery</h2>
+      {todos?.map((todo) => {
+        return <TodoCard key={todo.id} todo={todo} />;
+      })}
+    </div>
+  );
 };
