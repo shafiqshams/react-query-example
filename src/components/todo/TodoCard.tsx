a46bef98c71bfc6c3f725bb74a3c5987fc0548e0
Shafiq Shams <shafiqshamsessani@gmail.com>
2025-08-09 01:49:41 +0200

    chore: update import and move to folder

--- src/components/todo/TodoCard.tsx
+++ src/components/todo/TodoCard.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import type { Todo } from "../types/Todo";
+import type { Todo } from "../../types/Todo";
 
 export const TodoCard = ({ todo }: { todo: Todo }) => {
   const [checked, setChecked] = useState(todo.completed);
