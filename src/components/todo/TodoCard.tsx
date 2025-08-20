720d7b13236841b891cc5f531655252524f8d7ae
Shafiq Shams <shafiqshamsessani@gmail.com>
2025-08-20 16:41:27 +0200

    refactor: add destructuring for ui component

--- src/components/todo/TodoCard.tsx
+++ src/components/todo/TodoCard.tsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
 import type { Todo } from "../../types/Todo";
 
-export const TodoCard = ({ todo }: { todo: Todo }) => {
-  const [checked, setChecked] = useState(todo.completed);
+interface TodoCardProps {
+  todo: Todo;
+}
+
+export const TodoCard = ({
+  todo: { todo: title, completed }, // <-- alias todo → title
+}: TodoCardProps) => {
+  const [checked, setChecked] = useState(completed);
 
   return (
     <div className="todo-card">
-      <p style={{ textDecoration: checked ? "line-through" : "" }}>
-        {todo.title}
-      </p>
+      <p style={{ textDecoration: checked ? "line-through" : "" }}>{title}</p>
       <input
         type="checkbox"
         checked={checked}
