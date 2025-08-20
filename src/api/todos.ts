25f9605da9cfeb78ff0e1ad97bcc5dace6415799
Shafiq Shams <shafiqshamsessani@gmail.com>
2025-08-20 16:32:55 +0200

    feat(api): add fetchTodos

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,32 +1,11 @@
-/* Mock functions for fetching todos */
-
-import { todos } from "../mock_data/todos";
+import { fetcher } from "../lib/fetcher";
 import type { Todo } from "../types/Todo";
 
-export const fetchTodos = async (searchQuery: string = "") => {
-  //Simulate actual fetching from the API
-  await new Promise((resolve) => setTimeout(resolve, 100));
-
-  console.log("Fetching Todos");
-  const filteredTodos = todos.filter((todo) =>
-    todo.title.toLowerCase().includes(searchQuery.toLowerCase())
-  );
-
-  return [...filteredTodos];
+type TodoResponse = {
+  todos: Todo[];
+  total: number;
+  skip: number;
+  limit: number;
 };
 
-export const addTodo = async (todo: Pick<Todo, "title">): Promise<Todo> => {
-  //Simulate actual fetching from the API
-  await new Promise((resolve) => setTimeout(resolve, 100));
-
-  const newTodo: Todo = {
-    id: todos.length + 1,
-    title: todo.title,
-    completed: false,
-  };
-
-  // This is just to store in memory for the current execution
-  todos.push(newTodo);
-
-  return newTodo;
-};
+export const fetchTodos = () => fetcher<TodoResponse>("/todos");
