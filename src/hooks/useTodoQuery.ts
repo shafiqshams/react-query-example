3582cec661315b69fd76b921b89682d6faeba68f
Shafiq Shams <shafiqshamsessani@gmail.com>
2025-08-20 16:35:31 +0200

    refactor: add fetchTods and rm add todo for now

--- src/hooks/useTodoQuery.ts
+++ src/hooks/useTodoQuery.ts
@@ -1,21 +1,11 @@
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { addTodo, fetchTodos } from "../api/todos";
+import { useQuery } from "@tanstack/react-query";
+import { fetchTodos } from "../api/todos";
 
 export const useTodos = () => {
   return useQuery({
     queryKey: ["todos"],
-    queryFn: () => fetchTodos(),
-    // staleTime: Infinity,
+    queryFn: fetchTodos,
+    staleTime: Infinity,
     // gcTime: 0, never cache the data at all, and always fetch new data
-  });
-};
-
-export const useAddTodo = () => {
-  const qc = useQueryClient();
-  return useMutation({
-    mutationFn: addTodo,
-    onSuccess: () => {
-      qc.invalidateQueries({ queryKey: ["todos"] });
-    },
   });
 };
