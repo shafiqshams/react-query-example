7e548e236515f7b1141e091198df5c878c6491e8
Shafiq Shams <shafiqshamsessani@gmail.com>
2025-08-20 16:40:47 +0200

    refactor: add TodoListProps that contains todos as Todo[]

--- src/components/todo/TodoList.tsx
+++ src/components/todo/TodoList.tsx
@@ -1,10 +1,14 @@
 import type { Todo } from "../../types/Todo";
 import { TodoCard } from "./TodoCard";
 
-export const TodoList = ({ todos }: { todos: Todo[] | undefined }) => {
+interface TodoListProps {
+  todos: Todo[];
+}
+
+export const TodoList = ({ todos }: TodoListProps) => {
   return (
     <div className="todo-list">
-      {todos?.map((todo) => {
+      {todos.map((todo) => {
         return <TodoCard key={todo.id} todo={todo} />;
       })}
     </div>
