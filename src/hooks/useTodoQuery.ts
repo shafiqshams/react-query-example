45a5de65ac7a2820abd454cf7e19320bc08094f6
Shafiq Shams <shafiqshamsessani@gmail.com>
2025-08-21 14:30:19 +0200

    feat: use react query to post data to add mutation

--- src/hooks/useTodoQuery.ts
+++ src/hooks/useTodoQuery.ts
@@ -1,5 +1,5 @@
-import { useQuery } from "@tanstack/react-query";
-import { fetchTodos } from "../api/todos";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { fetchTodos, postTodo, type TodoResponse } from "../api/todos";
 
 export const useTodos = () => {
   return useQuery({
@@ -7,5 +7,20 @@ export const useTodos = () => {
     queryFn: fetchTodos,
     staleTime: Infinity,
     // gcTime: 0, never cache the data at all, and always fetch new data
+  });
+};
+
+export const useAddTodo = () => {
+  const qc = useQueryClient();
+  return useMutation({
+    mutationFn: postTodo,
+    onSuccess: (newTodo) => {
+      qc.setQueryData(["todos"], (oldData: TodoResponse) => {
+        return {
+          ...oldData,
+          todos: [newTodo, ...oldData.todos],
+        };
+      });
+    },
   });
 };
