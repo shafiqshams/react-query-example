26660131bd312c0e93dbbc1e1b3539adbecdef31
Shafiq Shams <shafiqshamsessani@gmail.com>
2025-08-21 14:29:20 +0200

    feat(api): add post todo request

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,7 +1,7 @@
 import { fetcher } from "../lib/fetcher";
 import type { Todo } from "../types/Todo";
 
-type TodoResponse = {
+export type TodoResponse = {
   todos: Todo[];
   total: number;
   skip: number;
@@ -9,3 +9,20 @@ type TodoResponse = {
 };
 
 export const fetchTodos = () => fetcher<TodoResponse>("/todos");
+
+//TODO: Refactor
+export const postTodo = async (title: string): Promise<Todo> => {
+  const todo: Todo = {
+    id: Date.now(),
+    todo: title,
+    completed: false,
+    // userId: Math.random(1,2)
+  };
+
+  const newTodo: Todo = await fetcher("/todos/add", {
+    method: "POST",
+    body: JSON.stringify({ ...todo, userId: 5 }),
+  });
+
+  return newTodo;
+};
